a140f39c82efb0c7fdac898c77f036dd4132d84b
Alex Thompson <37565509+aemtea@users.noreply.github.com>
2021-09-04 15:29:02 -0500

    Implement sample discord login

--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
-console.log('Hello Void');
+// Require the necessary discord.js classes
+import { Client, Intents } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(process.env.TOKEN).then(() => {
+    console.log('Logged in!')
+}).catch((err) => {
+    console.log(err)
+});
