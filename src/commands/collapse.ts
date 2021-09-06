31022e15a4fa13cebde725bd98e3e1431bef2506
Alex Thompson <37565509+aemtea@users.noreply.github.com>
2021-09-06 12:07:29 -0500

    Add follow up messages & delete void on /collapse

--- src/commands/collapse.ts
+++ src/commands/collapse.ts
@@ -1,11 +1,59 @@
+import util from 'util';
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, InteractionReplyOptions, ThreadChannel } from 'discord.js';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const wait = util.promisify(setTimeout);
+
+const theVoidString = 'the-void';
+const token: string = process.env.TOKEN!;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('collapse')
         .setDescription('Collapses the void into nothingness.'),
     async execute(interaction: CommandInteraction) {
-        await interaction.reply('The void begins to rumble...');
+        const rest = new REST({ version: '9' }).setToken(token);
+        const theVoid = interaction.guild?.channels.cache.find(x => x.name === theVoidString);
+
+        if (!theVoid) {
+            await interaction.reply(<InteractionReplyOptions>{
+                content: 'There is no void to collapse.',
+                ephemeral: true
+            });
+            return;
+        }
+        const beginRumbling = 'The void begins to rumble...';
+        await interaction.reply(<InteractionReplyOptions>{
+            content: beginRumbling,
+            ephemeral: true
+        });
+        (<ThreadChannel>theVoid).send(beginRumbling);
+        await wait(10000);
+
+        const rumblingIntensifies = 'Rumbling intensifies...';
+        await interaction.followUp(<InteractionReplyOptions>{
+            content: beginRumbling,
+            ephemeral: true
+        });
+        (<ThreadChannel>theVoid).send(rumblingIntensifies);
+        await wait(10000);
+
+        try {
+            await rest.delete(Routes.channel(theVoid.id));
+
+            if (interaction.channelId != theVoid.id) {
+                await interaction.followUp(<InteractionReplyOptions>{
+                    content: 'The void vanishes without a trace.',
+                    ephemeral: true
+                });
+            }
+        } catch (err) {
+            await interaction.followUp('The void stabilizes unexpectedly.');
+            console.log(err);
+        }
     },
 };
