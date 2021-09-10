d4ffa8ad27fd7e985a3af9368068438510454202
Alex Thompson <37565509+aemtea@users.noreply.github.com>
2021-09-10 17:03:45 -0500

    Added option to collapse immediately with prompt

--- src/commands/collapse.ts
+++ src/commands/collapse.ts
@@ -1,7 +1,7 @@
 import util from 'util';
 import dotenv from 'dotenv';
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction, InteractionReplyOptions } from 'discord.js';
+import { ButtonInteraction, Collection, CommandInteraction, InteractionDeferReplyOptions, InteractionDeferUpdateOptions, InteractionReplyOptions, MessageActionRow, MessageButton } from 'discord.js';
 import { VoidInteractionUtils } from '../utils/voidInteractionUtils';
 
 dotenv.config();
@@ -10,7 +10,11 @@ const wait = util.promisify(setTimeout);
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('collapse')
-        .setDescription('Collapses the void into nothingness.'),
+        .setDescription('Collapses the void into nothingness.')
+        .addBooleanOption(option =>
+            option.setName('immediate')
+                .setDescription('Immediately collapses the void.')
+                .setRequired(false)),
     async execute(interaction: CommandInteraction) {
         try {
             var theVoid = VoidInteractionUtils.getVoidChannel(interaction);
@@ -22,29 +26,72 @@ module.exports = {
                 });
                 return;
             }
-            const beginRumbling = 'The void begins to rumble...';
-            await interaction.reply(<InteractionReplyOptions>{
-                content: beginRumbling,
+
+            await interaction.deferReply(<InteractionDeferReplyOptions>{
                 ephemeral: true
             });
-            await theVoid.send(beginRumbling);
-            await wait(10000);
 
-            const rumblingIntensifies = 'Rumbling intensifies...';
-            await interaction.followUp(<InteractionReplyOptions>{
-                content: rumblingIntensifies,
-                ephemeral: true
-            });
-            await theVoid.send(rumblingIntensifies);
-            await wait(10000);
+            var immediate = interaction.options.getBoolean('immediate');
 
-            await theVoid.delete();
+            if (immediate) {
+                const row = new MessageActionRow()
+                    .addComponents(
+                        new MessageButton()
+                            .setCustomId('collapseCancel')
+                            .setLabel('Cancel')
+                            .setStyle(2) //SECONDARY
+                    )
+                    .addComponents(
+                        new MessageButton()
+                            .setCustomId('collapseImmediate')
+                            .setLabel('Collapse')
+                            .setStyle(4) //DANGER
+                    );
 
-            if (interaction.channelId != theVoid.id) {
-                await interaction.followUp(<InteractionReplyOptions>{
-                    content: 'The void vanishes without a trace.',
+                await interaction.editReply(<InteractionReplyOptions>{
+                    content: 'Are you sure you want to collapse the void?',
+                    ephemeral: true,
+                    components: [row]
+                });
+
+                const filter = (i: ButtonInteraction) => i.user.id === interaction.user.id;
+                const collector = interaction.channel?.createMessageComponentCollector({ filter, time: 15000 });
+
+                collector?.on('collect', async (i: ButtonInteraction) => {
+                    if (i.customId === 'collapseImmediate') {
+                        await theVoid!.delete();
+                        await i.update(<InteractionDeferUpdateOptions>{ content: 'The void vanishes without a trace.', components: [] })
+                    } else if (i.customId === 'collapseCancel') {
+                        await i.update(<InteractionDeferUpdateOptions>{ content: 'The void stabilizes.', components: [] })
+                    }
+                });
+
+                collector?.on('end', (collected: Collection<string, ButtonInteraction>) => console.log(`Collectoed ${collected.size} items`));
+            } else {
+                const beginRumbling = 'The void begins to rumble...';
+                await interaction.editReply(<InteractionReplyOptions>{
+                    content: beginRumbling,
                     ephemeral: true
                 });
+                await theVoid.send(beginRumbling);
+                await wait(10000);
+
+                const rumblingIntensifies = 'Rumbling intensifies...';
+                await interaction.followUp(<InteractionReplyOptions>{
+                    content: rumblingIntensifies,
+                    ephemeral: true
+                });
+                await theVoid.send(rumblingIntensifies);
+                await wait(10000);
+
+                await theVoid.delete();
+
+                if (interaction.channelId != theVoid.id) {
+                    await interaction.followUp(<InteractionReplyOptions>{
+                        content: 'The void vanishes without a trace.',
+                        ephemeral: true
+                    });
+                }
             }
         } catch (err) {
             await interaction.followUp(<InteractionReplyOptions>{
