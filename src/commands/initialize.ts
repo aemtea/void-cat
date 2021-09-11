91881748f0e956492ac80dac6cfa5b4adaa2356a
Alex Thompson <37565509+aemtea@users.noreply.github.com>
2021-09-11 09:38:28 -0500

    Minor refactor of command modules

--- src/commands/initialize.ts
+++ src/commands/initialize.ts
@@ -1,43 +1,39 @@
-import dotenv from 'dotenv';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction, InteractionDeferReplyOptions, InteractionReplyOptions } from 'discord.js';
 import { VoidInteractionUtils } from '../utils/voidInteractionUtils';
 
-dotenv.config();
+export const data = new SlashCommandBuilder()
+    .setName('initialize')
+    .setDescription('Reaches into the nothingness and returns with the void.');
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('initialize')
-        .setDescription('Reaches into the nothingness and returns with the void.'),
-    async execute(interaction: CommandInteraction) {
-        try {
-            const theVoid = VoidInteractionUtils.getVoidChannel(interaction);
-            const initializeString = 'You stare into the void. The void stares back at you.';
+export const execute = async (interaction: CommandInteraction) => {
+    try {
+        const theVoid = VoidInteractionUtils.getVoidChannel(interaction);
+        const initializeString = 'You stare into the void. The void stares back at you.';
 
-            if (theVoid) {
-                await interaction.reply(<InteractionReplyOptions>{
-                    content: initializeString,
-                    ephemeral: true
-                });
-                return;
-            }
-
-            await interaction.deferReply(<InteractionDeferReplyOptions>{
-                ephemeral: true
-            });
-
-            await VoidInteractionUtils.createVoidChannel(interaction);
-
-            await interaction.editReply(<InteractionReplyOptions>{
+        if (theVoid) {
+            await interaction.reply(<InteractionReplyOptions>{
                 content: initializeString,
                 ephemeral: true
             });
-        } catch (err) {
-            await interaction.followUp(<InteractionReplyOptions>{
-                content: 'You call out to the void and hear nothing in return.',
-                ephemeral: true
-            });
-            console.log(err);
+            return;
         }
+
+        await interaction.deferReply(<InteractionDeferReplyOptions>{
+            ephemeral: true
+        });
+
+        await VoidInteractionUtils.createVoidChannel(interaction);
+
+        await interaction.editReply(<InteractionReplyOptions>{
+            content: initializeString,
+            ephemeral: true
+        });
+    } catch (err) {
+        await interaction.followUp(<InteractionReplyOptions>{
+            content: 'You call out to the void and hear nothing in return.',
+            ephemeral: true
+        });
+        console.log(err);
     }
-};
+}
--- src/commands/speak.ts
+++ src/commands/speak.ts
@@ -1,46 +1,42 @@
-import util from 'util';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction, InteractionDeferReplyOptions, InteractionReplyOptions } from "discord.js";
 import { VoidInteractionUtils } from "../utils/voidInteractionUtils";
 
-const wait = util.promisify(setTimeout);
+export const data = new SlashCommandBuilder()
+    .setName('speak')
+    .setDescription('Speak through the Void Cat.')
+    .addStringOption(option =>
+        option.setName('incantation')
+            .setDescription('The incantation to speak.')
+            .setRequired(true));
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('speak')
-        .setDescription('Speak through the Void Cat.')
-        .addStringOption(option =>
-            option.setName('incantation')
-                .setDescription('The incantation to speak.')
-                .setRequired(true)),
-    async execute(interaction: CommandInteraction) {
-        try {
-            var theVoid = VoidInteractionUtils.getVoidChannel(interaction);
+export const execute = async (interaction: CommandInteraction) => {
+    try {
+        var theVoid = VoidInteractionUtils.getVoidChannel(interaction);
 
-            if (!theVoid) {
-                await interaction.reply(<InteractionReplyOptions>{
-                    content: 'There is no void to speak through.',
-                    ephemeral: true
-                });
-                return;
-            }
-            await interaction.deferReply(<InteractionDeferReplyOptions>{
+        if (!theVoid) {
+            await interaction.reply(<InteractionReplyOptions>{
+                content: 'There is no void to speak through.',
                 ephemeral: true
             });
-            var incantation = interaction.options.getString('incantation');
-
-            await theVoid.send(incantation!);
-
-            await interaction.editReply(<InteractionReplyOptions>{
-                content: 'Your incantation has been spoken.',
-                ephemeral: true
-            });
-        } catch (err) {
-            await interaction.editReply(<InteractionReplyOptions>{
-                content: 'The void refuses your incantation.',
-                ephemeral: true
-            });
-            console.log(err);
+            return;
         }
-    },
-};
+        await interaction.deferReply(<InteractionDeferReplyOptions>{
+            ephemeral: true
+        });
+        var incantation = interaction.options.getString('incantation');
+
+        await theVoid.send(incantation!);
+
+        await interaction.editReply(<InteractionReplyOptions>{
+            content: 'Your incantation has been spoken.',
+            ephemeral: true
+        });
+    } catch (err) {
+        await interaction.editReply(<InteractionReplyOptions>{
+            content: 'The void refuses your incantation.',
+            ephemeral: true
+        });
+        console.log(err);
+    }
+}
